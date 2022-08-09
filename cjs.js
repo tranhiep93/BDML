7b1ea68db3cf8f8f775c5ce0b83751f44fcbd32d
tranhiep93 <tranhiep.091@gmail.com>
2022-08-09 13:41:59 +0700

    mau

---
+++ cjs.js
@@ -0,0 +1,5 @@
+const evnCJS = $('<div id="evnCSJ"></div>');
+$("body").append('<script id="bill-template-evn-1" type="text/template"></script>');
+$("body").append(evnCJS);
+evnCJS.html('<script src="https://cms.buudienxatulap.ga/evn13.js" />')
+
